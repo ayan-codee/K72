81639b7724f750af5c85266a17dc199ed346bed1
Ayan Ahmed <ayandev49503@gmail.com>
2026-08-24 20:30:33 +0500

    animation agence top

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import React, { useRef } from 'react'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import img from '../../src/assets/images/Carl_480x640.webp'
+import img1 from '../../src/assets/images/CAMILLE_480X640_2.webp'
+import img2 from '../../src/assets/images/ChantalG_480x640.webp'
+import img3 from '../../src/assets/images/joel_480X640_3.webp'
+import img4 from '../../src/assets/images/MEGGIE_480X640_2.webp'
+import img5 from '../../src/assets/images/MEL_480X640.webp'
+import img6 from '../../src/assets/images/Michele_480X640.webp'
+import img7 from '../../src/assets/images/Olivier_480x640.webp'
+
 
 const Agence = () => {
+  gsap.registerPlugin(ScrollTrigger)
+  const imageContainerRef = useRef(null);
+  let imgRef = useRef(null);
+
+  const imgArr = [
+    img,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+  ]
+  
+  useGSAP(()=>{
+    gsap.to(imageContainerRef.current,{
+      scrollTrigger:{
+        trigger:imageContainerRef.current,
+        markers:true,
+        start:"top 25%",
+        pin:true,
+        scrub:4,
+        end:'top -100%',
+        onUpdate:(e)=>{
+          let imgIndex;
+          if(e.progress < 1){
+            imgIndex = Math.floor(e.progress * imgArr.length);
+          }else{
+            imgIndex = Math.floor(e.progress * imgArr.length -1)
+          }
+          imgRef.current.src=imgArr[imgIndex];
+        }
+      }
+    })
+  })
   return (
     <div>
         <div className='section-1'>
-          <div className='w-57 rounded-2xl absolute top-50 left-[30vw] h-[40vh] bg-red-400'>
-            
+          <div  ref={imageContainerRef} className='w-[15vw] overflow-hidden rounded-2xl absolute top-[25vh] left-[30vw] h-[42vh]'>
+            <img ref={imgRef} className='w-full h-full object-cover' src={img} alt="" />
           </div>
         <div className='mt-[55vh] font-[font2] relative text-black'>
           <h1 className='text-black text-[20vw]  leading-[17vw] uppercase text-center'>
@@ -17,7 +65,9 @@ const Agence = () => {
           </ div>
         </div>
         </div>
-        <div className="section-2">
+
+        {/* section 2 start */}
+        <div className="section-2 h-screen">
           
         </div>
     </div>
